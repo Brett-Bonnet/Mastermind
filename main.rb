9bfef3b96730e137468f62054ab135a8cb3c2b7d
Brett Bonnet <brettbonnet4@gmail.com>
2019-07-30 10:02:11 -0400

    Added greet language and valid_guess? check

--- main.rb
+++ main.rb
@@ -1,3 +1,4 @@
+
 require './tools.rb'
 require './color.rb'
 require './code.rb'
@@ -9,8 +10,6 @@ include Tools
   def initialize
     reset
     greet
-    get_code
-    play
   end
 
   def reset
@@ -21,8 +20,28 @@ include Tools
 
   def greet
     puts "Welcome to Mastermind!"
+    puts "Mastermind is a code-breaking game designed for two players."
+    puts "The Codemaker creates a secret code (in this version, a 5 digit number consisting of numbers 1-5) and the codebreaker has 12 attempts to crack the code"
+    puts "After making a guess, the codebreaker will receive clues as to which parts are correct and which are wrong:\n\n"
+    puts "Numbers in \e[32mgreen\e[0m are correct!" 
+    puts "Numbers in \e[31mred\e[0m are incorrect"
+    puts "Numbers in \e[34mblue\e[0m are part of the secret code, however in a different place.\n\n"
+    get_game_type
+  end
+  def get_game_type
+    puts "*************************************************************************"
+    puts "First, you'll need to pick whether you will play as the codebreaker, or the codemaker!"
     puts "Would you like to be the codebreaker (Type 1) or the codemaker (Type 2)"
-    @game_type = gets.chomp.to_i
+  i = 0  
+  while i < 5 do
+     @game_type = gets.chomp.to_i
+     if @game_type == 1 || @game_type == 2
+      get_code   
+     else puts "That's not a valid game type! Type 1 or 2 and press enter to play."
+       i += 1
+       redo
+     end
+  end
   end
 
   def play
@@ -30,11 +49,17 @@ include Tools
     while @counter < 12 do 
       @guess = []
       get_guess
+     if valid_guess?(@guess_attempt)
+       @guess = @guess_attempt
+     else puts "That's not a valid guess! Try again!"
+     redo
+     end
     break if winner?
       check_guess(@guess, @code)
-      
-    
-    
+    if @counter == 11
+    puts "Better luck next time!"
+    play_again?
+    end
     @counter += 1
     end
   end
@@ -42,6 +67,7 @@ include Tools
   def get_code
     @new_code = Code.new(@game_type)
     @code = @new_code.display
+    play
   end
 
   def winner?
@@ -72,10 +98,6 @@ end
 
 
 play = Game.new
-
-
-
-
 
 
 
